1c13fa51c89a595726d1dc8032407cbdb2dcf9ec
thepudding <thepudding@users.noreply.github.com>
2024-05-20 23:49:33 -0700

    fixes overflow for send example

--- examples/Send/Send.ino
+++ examples/Send/Send.ino
@@ -4,17 +4,16 @@
 #define REPEAT_COMMAND 8
 #define DEBUG true
 
-uint32_t message = 0;
-
 Bofu::Transmit bofu = Bofu::Transmit();
 
-void readMessage() {
-  message = 0;
+Bofu::Message readMessage() {
+  uint32_t message = 0;
   byte message_bytes[4];
   Serial.readBytes(message_bytes, 4);
   for(int i = 0; i < 4; i++) {
-    message += message_bytes[3 - i] << (i*8);
+    message += static_cast<uint32_t>(message_bytes[3 - i]) << (i*8);
   }
+  return Bofu::Message(message);
 }
 
 void setup() {
@@ -29,11 +28,12 @@ void setup() {
 void loop()
 {
   if(Serial.available() >= 4) {
-    readMessage();
+    Bofu::Message m = readMessage();
 
     Serial.print("Sending Message: ");
-    Serial.print(message, HEX);
+    Serial.print(m.getChecksum(),HEX);
+    Serial.print(m.getData(), HEX);
     Serial.println("");
-    bofu.sendMessage(message);
+    bofu.sendMessage(m);
   }
 }
